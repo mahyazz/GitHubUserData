6d04d07d95c6abedb1e3bc0bbce22125c9d30ec5
mahyazz <mahya.zarrabi@gmail.com>
2025-04-01 16:30:20 +0330

    Improve caching

--- src/main/kotlin/cache/ApiCache.kt
+++ src/main/kotlin/cache/ApiCache.kt
@@ -1,16 +1,30 @@
 package cache
 
-import java.util.concurrent.ConcurrentHashMap
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.time.Instant
 
-class ApiCache<K, V> {
-    private val cache = ConcurrentHashMap<K, V>()
+// Cache lifetime: 5 mins
+class ApiCache<K, V>(private val expirationMillis: Long = 5 * 60 * 1000) { 
+    private val cache = mutableMapOf<K, Pair<V, Instant>>()
+    private val mutex = Mutex()
 
-    fun getOrFetch(key: K, fetcher: suspend () -> V): V {
-        return cache.computeIfAbsent(key) { runBlocking { fetcher() } }
+    suspend fun getOrFetch(key: K, fetcher: suspend () -> V): V {
+        mutex.withLock {
+            val cachedData = cache[key]
+            if (cachedData != null && Instant.now().isBefore(cachedData.second.plusMillis(expirationMillis))) {
+                return cachedData.first
+            }
+
+            val value = fetcher() 
+            cache[key] = value to Instant.now() 
+            return value
+        }
     }
 
-    fun getCachedKeys(): Set<K> = cache.keys 
+    fun getCachedKeys(): Set<K> = cache.keys
 
-    fun getCachedValue(key: K): V? = cache[key] 
+    fun getCachedValue(key: K): V? = cache[key]?.takeIf {
+        Instant.now().isBefore(it.second.plusMillis(expirationMillis))
+    }?.first
 }
